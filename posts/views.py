fb48c11ebb4059366fdcb6e869b83765ec2fbbef
Samuel Dominguez <onlinebusinessfy@gmail.com>
2025-01-11 12:03:27 -0800

    Class Assignment 2 FSDI 113

--- posts/views.py
+++ posts/views.py
@@ -3,7 +3,7 @@ from django.views.generic import(
     DetailView,
     CreateView,
     UpdateView,
-    DeleteView
+    DeleteView,
 )
 from .models import Post, Status
 from django.urls import reverse_lazy
@@ -43,9 +43,23 @@ class DraftPostListView(LoginRequiredMixin, ListView):
         )
         return context
 
-class PostDetailView(DetailView):
+class PostDetailView(UserPassesTestMixin, DetailView):
     template_name="posts/detail.html"
     model=Post
+
+    def test_func(self):
+        post=self.get_object()
+        if post.status.name=="published":
+            return True
+        elif post.status.name=="draft":
+            if (self.request.user.is_authenticated 
+                    and self.request.user==post.author):
+                return True
+        elif (post.status.name=="archived" 
+                and self.request.user.is_authenticated):
+            return True
+        else:
+            return False
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     template_name="posts/new.html"
@@ -73,3 +87,18 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     def test_func(self):
         post=self.get_object()
         return post.author==self.request.user
+    
+class PostArchieveView(LoginRequiredMixin, ListView):
+    template_name="posts/archived.html"
+    model=Post
+
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        archived=Status.objects.get(name="archived")
+        context["title"]="Archived"
+        context["post_list"]=(
+            Post.objects
+            .filter(status=archived)
+            .order_by("created_on").reverse()
+        )
+        return context
